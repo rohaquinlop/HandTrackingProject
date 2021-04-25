94c2b62dee43a6a55cdc18fc1ee682d075b5458a
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2021-04-25 15:44:53 -0500

    Update HandTrackingModule, fixed errors

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -1,9 +1,8 @@
 import cv2
 import mediapipe as mp
-import time
 
 
-class HandDetector():
+class HandDetector:
     def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
         self.mode = mode
         self.maxHands = maxHands
@@ -13,13 +12,12 @@ class HandDetector():
         self.mpHands = mp.solutions.hands
         self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
+        self.results = None
 
     def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
         self.results = self.hands.process(imgRGB)
-
-        # print(results.multi_hand_landmarks)
 
         if self.results.multi_hand_landmarks:
             for handLms in self.results.multi_hand_landmarks:
@@ -28,42 +26,16 @@ class HandDetector():
 
         return img
 
-    def findPosition(self, img, handNumber=0, draw = True):
+    def findPosition(self, img, handNumber=0, draw=True):
         lmLst = []
 
         if self.results.multi_hand_landmarks:
             hand = self.results.multi_hand_landmarks[handNumber]
-            for id, lm in enumerate(hand.landmark):
+            for idEnum, lm in enumerate(hand.landmark):
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                lmLst.append([id, cx, cy])
+                lmLst.append([idEnum, cx, cy])
                 if draw:
                     cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
 
         return lmLst
-
-
-def main():
-    pTime = 0
-    cTime = 0
-    cap = cv2.VideoCapture(1)
-    detector = HandDetector()
-
-    while True:
-        success, img = cap.read()
-
-        img = detector.findHands(img)
-        lmLst = detector.findPosition(img, draw=False)
-
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
-
-        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 0, 255), 3)
-
-        cv2.imshow("image", img)
-        cv2.waitKey(1)
-
-
-if __name__ == "__main__":
-    main()
